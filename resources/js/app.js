5a2c83728fd596b258cedce94b69a45a799b7590
Aeschna <134966047+Aeschna@users.noreply.github.com>
2024-09-05 13:37:28 +0300

    develop 05/09

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,13 +12,15 @@ const app2 = createApp(App2);
 
 // Set the token from local storage if it exists
 const token = localStorage.getItem('token');
-
+console.log(token);
 
 axios.defaults.withCredentials = true; // Send cookies with requests
 axios.defaults.baseURL = 'http://localhost:8000';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.api_token;
+
+
 
 app.use(router);
 
